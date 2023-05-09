2db9d4232b8b8980538ab5fd58ed5744368b9fb4
gonzam77 <gonzam77@gmial.com>
2023-05-09 19:46:43 -0300

    navbar and home created

---
+++ client/src/Pages/Home/home.jsx
@@ -0,0 +1,17 @@
+import styles from "./home.module.css";
+
+export default function Home() {
+    return (
+        <div>
+            <div className={styles.paginate}>
+
+            </div>
+            <div className={styles.selectors}>
+
+            </div>
+            <div className={styles.cards}>
+                
+            </div>
+        </div>
+    );
+};
