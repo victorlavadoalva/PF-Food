6be5cb4d7a9f376840dbbddc648edb9b1ab960ba
gonzam77 <gonzam77@gmial.com>
2023-05-10 10:54:19 -0300

    descripcion de componentes faltantes

--- client/src/Pages/Home/index.jsx
+++ client/src/Pages/Home/index.jsx
@@ -4,13 +4,13 @@ export default function Home() {
     return (
         <div>
             <div className={styles.paginate}>
-
+                <span>ACA VA EL PAGINADO</span>
             </div>
             <div className={styles.selectors}>
-
+                <span>ACA VAN LOS SELECTORES</span>
             </div>
             <div className={styles.cards}>
-                
+                <span>ACA VAN LAS CARTAS</span>
             </div>
         </div>
     );
