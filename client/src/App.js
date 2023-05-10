c3c3afa5bf69a2d4518f666c8f7157168e1e8a66
gonzam77 <gonzam77@gmial.com>
2023-05-10 19:59:38 -0300

    base main

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,12 @@
-import { Routes, Route } from "react-router-dom";
+
 import './App.css';
-import Home from './pages/Home/index'
-import Landing from "./pages/Landing/index";
-import NavBar from "./components/NavBar/index";
+
 // importar componentes
 function App() {
   return (
-    <div className="App">
-      <header></header>
-      <NavBar/>
-      <main>
-      <Routes>
-        <Route exact path='/' element ={Landing}/>
-        <Route exact path='/home' element ={Home}/> 
-      </Routes>
-      </main>
-      <footer></footer>
-    </div>
-  );
+    <>
+    </>
+  )
 }
 
 export default App;
