07a89aed631ceff3c855d3c8e875d92b43794b12
Kris0707 <francokris005@gmail.com>
2023-05-12 13:01:24 -0300

    Paginado Resuelto , y arreglo del Filtrado(CSS)

--- client/src/Components/Paginado/index.jsx
+++ client/src/Components/Paginado/index.jsx
@@ -1,12 +1,19 @@
 import Pagination from '@mui/material/Pagination';
 import Stack from '@mui/material/Stack';
 import * as React from 'react';
-
+import { useDispatch } from "react-redux";
+import { getRestorants } from "../../Redux/actions";
 export default function PaginationRounded() {
+
+  const dispatch = useDispatch()
+
+const handleChange = (e,p) => {
+  dispatch(getRestorants(p))
+}
 
   return (
     <Stack spacing={2}>
-      <Pagination count={10} variant="outlined" shape="rounded" />
+      <Pagination count={10} variant="outlined" onChange={handleChange} shape="rounded" />
     </Stack>
   );
 }
