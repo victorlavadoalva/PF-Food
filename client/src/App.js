a00ca867b0a6c96596be9dd60bf5b4ded7dd54bc
victor <victor.lavado15@hotmail.com>
2023-05-16 11:42:46 -0500

    agregado el auth0

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import { Route, Routes, useLocation } from "react-router-dom";
-import './App.css';
+import "./App.css";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import Error404 from "./Pages/404";
@@ -7,24 +7,25 @@ import Detail from "./Pages/Detail/index";
 import Form from "./Pages/Form";
 import Home from "./Pages/Home";
 import Landing from "./Pages/Landing/index.jsx";
+import Login from "./Pages/Login";
 function App() {
-
-  const location = useLocation()
+  const location = useLocation();
 
   return (
     <div className="app">
-      {
-       location.pathname !== "/" && <header>
-        <Header/>
-      </header>
-      }
+      {location.pathname !== "/" && (
+        <header>
+          <Header />
+        </header>
+      )}
       <main className="main">
         <Routes>
-          <Route exact path='/' element={<Landing />} />
-          <Route exact path='/home' element={<Home />} />
-          <Route exact path='/form' element={<Form />} />
-          <Route exact path='/detail/:restoId' element={<Detail />} />
-          <Route path="*" element={<Error404/>}/>
+          <Route exact path="/" element={<Landing />} />
+          <Route exact path="/home" element={<Home />} />
+          <Route exact path="/form" element={<Form />} />
+          <Route exact path="/detail/:restoId" element={<Detail />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route path="*" element={<Error404 />} />
         </Routes>
       </main>
       <footer className="footer">
@@ -35,4 +36,3 @@ function App() {
 }
 
 export default App;
-
--- client/src/index.js
+++ client/src/index.js
@@ -1,23 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+import { Auth0Provider } from "@auth0/auth0-react";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-import App from './App';
-import store from './Redux/store';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+import App from "./App";
+import store from "./Redux/store";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
+const domain = "dev-atgvksgdajxyslw7.us.auth0.com";
+const clientId = "iPgcv9EBf22z4LBco58RZpuzvqHPYKHR";
 
-  ReactDOM.render(
-    <Provider store={store}>
+ReactDOM.render(
+  <Provider store={store}>
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      redirectUri={window.location.origin} // es para retornar al usuario en la ruta que estaba
+    >
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </Provider>,
-      
-    document.getElementById('root')
-  );
+    </Auth0Provider>
+  </Provider>,
 
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
