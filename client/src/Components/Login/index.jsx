3c81a10ad36fd142d5cb5c2bdb01ef37a0e8d380
Kris0707 <francokris005@gmail.com>
2023-05-20 16:35:05 -0300

    sin espacios

--- client/src/Components/Login/index.jsx
+++ client/src/Components/Login/index.jsx
@@ -3,14 +3,13 @@ import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { GetUserEmail, Loading } from "../../Redux/actions";
 import { useAuth0 } from "@auth0/auth0-react";
-import styles from "./styles.module.css"
-export default function Login_Register() {
-  const { isAuthenticated, user, loginWithRedirect, logout } = useAuth0();
+import styles from "./styles.module.css";
 
-    
+export default function Login_Register() {
+  const { isAuthenticated, user, loginWithPopup, logout } = useAuth0();
 
   const handleLogin = () => {
-    window.localStorage.setItem('redirectPath', window.location.pathname);
+    window.localStorage.setItem("redirectPath", window.location.pathname);
     loginWithRedirect();
   };
 
@@ -28,7 +27,12 @@ export default function Login_Register() {
         </div>
       ) : (
         <div>
-            <button onClick={() => handleLogin()} className={styles.buttonAccount}>Login</button>
+          <button
+            onClick={() => handleLogin()}
+            className={styles.buttonAccount}
+          >
+            Login
+          </button>
         </div>
       )}
     </>
