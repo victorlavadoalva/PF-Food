99b5b3099961b91631ab8cff108bb70c551cb893
Kris0707 <francokris005@gmail.com>
2023-05-24 20:17:58 -0300

    Ruta Cliente terminada

--- client/src/Components/RutasProtegidas/RutasCliente.jsx
+++ client/src/Components/RutasProtegidas/RutasCliente.jsx
@@ -3,17 +3,23 @@ import { useNavigate } from "react-router-dom";
 import { useState,useEffect } from "react";
 
 export default function RutasCliente(){
-    const redirection = "/restorant"
+    const redirection = "/home"
     const navigate = useNavigate()
     const dataUser = window.localStorage.getItem("UserLogVerificate")
     const [shouldRedirect, setShouldRedirect] = useState(false);
     
-
-    // if (shouldRedirect) {
-    //     alert("Inicia sesion con una cuenta Cliente");
-    //   navigate(redirection);
-    //   return null; // Evitar que se muestre la ruta antes de redirigir
-    // }
+    useEffect(() => {
+        if (type_customer !== "Cliente") {
+          
+          setShouldRedirect(true);
+        }
+      }, [dataUser]);
+      console.log("RedirectRuta", shouldRedirect)
+      if (shouldRedirect) {
+          alert("Inicia sesion para acceder a esta ruta");
+        navigate(redirection);
+        return null; // Evitar que se muestre la ruta antes de redirigir
+      }
     
     return <Outlet />;
 }
