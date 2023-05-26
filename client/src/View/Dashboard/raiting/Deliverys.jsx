07f5c59bc67020bc0590475ecf03b0ab91b01cae
Miguel Sierra <sierrabolanosmiguel@gmail.com>
2023-05-25 19:47:09 -0500

    Dashboards funcionales

--- client/src/View/Dashboard/raiting/Deliverys.jsx
+++ client/src/View/Dashboard/raiting/Deliverys.jsx
@@ -1,27 +1,21 @@
-import { Bar } from 'react-chartjs-2';
-import style from './ValorationsMonth.module.css';
+import { Line } from 'react-chartjs-2';
+import style from './Rating.module.css';
 
 const ValorationsMonth = (props) => {
 
     {/* INFORMACION DEL DASHBOARD */}
-    console.log(props);
 
     const data = {
         labels: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Sptiembre', 'Octubre', 'Noviembre', 'diciembre'],
         datasets: [
             {
-                label: 'Reseñas',
-                data: props.data.ratingPerMonth ? props.data.ratingPerMonth.stockPerMonth : [],
+                label: '# de Deliverys',
+                data: props.data.ratingPerMonth ? props.data.deliverysPerMonth : [],
                 backgroundColor: 'rgba(255, 99, 132, 0.5)',
                 borderColor: 'rgba(255, 99, 132, 1)',
                 borderWidth: 1,
-            },
-            {
-                label: 'Rating',
-                data: props.data.ratingPerMonth ? props.data.ratingPerMonth.ratingPerMonth : [],
-                backgroundColor: 'rgba(58, 80, 107, 0.5)',
-                borderColor: 'rgba(58, 80, 107, 1)',
-                borderWidth: 1,
+                pointRadius: 8,
+                pointHoverRadius: 6
             },
         ],
     };
@@ -47,8 +41,8 @@ const ValorationsMonth = (props) => {
     return(
         <div style={{ width: '1000px', height: '400px' }} className={style.container}>
             <center style={{height: '300px' }}>
-                <h2>Reseñas por mes</h2>
-                <Bar data={data} options={options} />
+                <h2>Deliverys y Reservas Mensuales</h2>
+                <Line data={data} options={options} />
             </center>
         </div>
     );
