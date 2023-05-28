0fd8434c030433271ef041f9ac54fc39bf2907e9
Kris0707 <francokris005@gmail.com>
2023-05-27 21:06:25 -0300

    Login UI

--- client/src/Components/Login/index.jsx
+++ client/src/Components/Login/index.jsx
@@ -2,14 +2,15 @@ import { useAuth0 } from "@auth0/auth0-react";
 import styles from "./styles.module.css";
 import {useNavigate} from "react-router-dom"
 import { useState } from "react";
+import AccountMenu from "./AcountMenu";
+
 
 export default function Login_Register() {
   const navigate = useNavigate()
   const { isAuthenticated, user, loginWithRedirect, logout ,isLoading} = useAuth0();
   const [PerfilActive, setPerfilActive] = useState(false)
-const handleAcount = () => {
-  navigate("/home/perfil")
-}
+ 
+
 
 const handleActivatePerfil = () => {
   if(PerfilActive === true){
@@ -19,12 +20,7 @@ const handleActivatePerfil = () => {
   }
 }
 
-  const handleLogOut = () => {
- window.localStorage.setItem("redirectPath", window.location.pathname);
-window.localStorage.removeItem("UserToken")
-window.localStorage.removeItem("UserLogVerificate")
-    logout()
-  }
+
   const handleLogin = () => {
     window.localStorage.setItem("redirectPath", window.location.pathname);
   
@@ -33,41 +29,13 @@ window.localStorage.removeItem("UserLogVerificate")
 
   return (
     <>
-      {isAuthenticated ? (
-        <div className={styles.divUserImage}>
-          <div>
-          <img
-            src={user.picture}
-            alt={user.name}
-            className={styles.imagePerfil}
-            onClick={() => handleActivatePerfil()}
-          />
-          </div>
-          {PerfilActive && 
-          <>
-          <div className={styles.perfilDesplegable}>
-            <div className={styles.perfilname}>
-              <p>{user.name}</p>
-            </div>
-        <div className={styles.selectperfil}>
-          <select>
-            <option>
-              <span onClick={() => handleAcount()}>
-                Cuenta
-              </span>
-            </option>
-            <option>
-              <span onClick={() => handleLogOut()}>Log out</span>
-            </option>
-          </select>
-          </div>
-          </div>
-          </>
-          }
-          
-          
-        </div>
+    {isAuthenticated ? (
+<>
+<AccountMenu  />
+</> 
       ) : (
+        <>
+
         <div className={styles.buttonAccount}>
           <button 
             className={styles.btn}
@@ -77,6 +45,7 @@ window.localStorage.removeItem("UserLogVerificate")
             {isLoading ? "Cargando" : "Login"}
           </button>
         </div>
+</>
       )}
     </>
   );
