7907e1857f43d2ab3b09c463ef98e8c4f1f47134
gonzam77 <gonzam77@gmial.com>
2023-05-30 19:49:18 -0300

    orders and reservs resto

--- client/src/Components/Pedidos/index.jsx
+++ client/src/Components/Pedidos/index.jsx
@@ -1,14 +1,30 @@
 import * as React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import * as actions from '../../Redux/actions';
 import Box from '@mui/material/Box';
 import { DataGrid } from '@mui/x-data-grid';
-import { pedidosColumns, pedidosRows } from '../../dataHardcodeo/constants'
+import { ordersColumns, ordersRows } from '../../dataHardcodeo/constants'
 
 
 export default function DataGridDemo() {
 
-  const rows = pedidosRows;
-  const columns  = pedidosColumns;
-  
+  const orders = useSelector(state => state.orders);
+  const dispatch = useDispatch();
+
+  const restDataStorage = window.localStorage.getItem('RestData');
+  const restData = JSON.parse(restDataStorage);
+  const restoId = restData.restaurant.id;
+
+  console.log("restDataId", restData.restaurant.id);
+  console.log("RestDataStorage:", restDataStorage);
+
+  React.useEffect(() => {
+    dispatch(actions.getOrders(restoId));
+  }, [dispatch, restoId]);
+
+  const rows = ordersRows;
+  const columns = ordersColumns;
+
   return (
     <Box sx={{ height: 400, width: '100%' }}>
       <DataGrid
--- client/src/Components/Reservas/index.jsx
+++ client/src/Components/Reservas/index.jsx
@@ -1,13 +1,30 @@
 import * as React from 'react';
+import { useDispatch, useSelector} from 'react-redux';
+import axios from 'axios';
+import * as actions from '../../Redux/actions';
 import Box from '@mui/material/Box';
 import { DataGrid } from '@mui/x-data-grid';
-import { reservasRows, reservasColumns } from '../../dataHardcodeo/constants'
+import { reservsRows, reservsColumns } from '../../dataHardcodeo/constants';
 
 
 export default function DataGridDemo() {
 
-    const rows = reservasRows;
-    const columns = reservasColumns;
+    const reservs = useSelector(state => state.reservs);
+    const dispatch = useDispatch();
+    
+    const restDataStorage = window.localStorage.getItem('RestData');
+    const restData = JSON.parse(restDataStorage);
+    const restoId = restData.restaurant.id;
+    
+    console.log("restDataId", restData.restaurant.id);
+    console.log("RestDataStorage:", restDataStorage);
+    
+    React.useEffect(()=>{
+        dispatch(actions.getReservs(restoId));
+    }, [dispatch, restoId]);
+
+    const rows = reservsRows;
+    const columns = reservsColumns;
 
     return (
         <Box sx={{ height: 400, width: '100%' }}>
